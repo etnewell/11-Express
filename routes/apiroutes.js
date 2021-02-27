73834b4db1d08cd6adf4e4f0627792c3d96713c5
etnewell <ethannewell@me.com>
2021-02-26 16:46:25 -0800

    made changes to overall app - should commit more

---
+++ routes/apiroutes.js
@@ -0,0 +1,36 @@
+const notes = require("../db/db");
+// console.log(notes);
+
+module.exports = (app) => {
+
+    app.get("/api/notes", (req, res) =>{
+        console.log("the api get request was called 4")
+        res.json(notes)
+        console.log(notes);
+
+    });
+    app.post("/api/notes", (req, res) =>{
+        console.log("the api post request was called 10")
+        var idNum = notes.length;
+        req.body.id = idNum;
+        console.log(idNum);
+        console.log(req.body);
+        notes.push(req.body);
+
+    });
+    app.delete("/api/notes/:id", (req, res) =>{
+        console.log("the api delete request was called 17")
+        var reqId = req.params.id;
+        var noteAry = [...notes.db];
+        console.log(reqId);
+        console.log(noteAry);
+        for (let [i, note] of notes.entries()) {
+            if (note.id === reqId) {
+                notes.splice(i, 1);
+            }
+        }
+        console.log(notes)
+
+
+    })
+};
